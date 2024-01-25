51a40a1939559f57249b1c25d740f7a5a0c003ce
Esther An <ean@college.harvard.edu>
2024-01-25 13:48:02 -0500

    working to-do list, will add css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, current_app
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,17 +10,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#### Debugging app_context()
+app.app_context().push()
+
+# from app import app
+# from app import db
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
+    # __table_args__ = {'extend_existing': True}
 
-@app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+
+@app.route("/")
 def home():
     todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
+    return render_template('base.html', todo_list=todo_list)
+
+@app.route("/add", methods=['POST'])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     db.create_all()
@@ -28,6 +56,3 @@ if __name__ == "__main__":
     # Reactivity?
     # No
     app.run(debug=True)
-
-
-
